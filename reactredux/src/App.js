783eec2b8d512d3bf1a52d3fa710e05f697f8053
田中 秀明 <tanaka_hideaki@aja-kk.co.jp>
2018-11-15 19:32:17 +0900

    view: update

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -8,7 +8,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <GameArea />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -23,6 +22,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <GameArea />
       </div>
     );
   }
